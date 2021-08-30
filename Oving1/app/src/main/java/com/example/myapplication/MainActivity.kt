8d0a7397659b5adb0be2f7fcba0fc7c67c37a159
Torbjørn Bakke <tbakke93@gmail.com>
2021-08-30 19:53:59 +0200

    buttons with functionality and textview finished

--- Oving1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Oving1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,18 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var personalName = ""
+    private var familyName = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun writePersonalName(view: View) {
-        Log.d("INFO", "Personal name!")
+        personalName = "Torbjørn"
+
+        findViewById<TextView>(R.id.nameDisplay).apply {
+            text = getName()
+        }
+
+        Log.d("WARNING", personalName)
     }
 
     fun writeFamilyName(view: View) {
-        Log.d("INFO", "Family name!")
+        familyName = "Bakke"
+
+        findViewById<TextView>(R.id.nameDisplay).apply {
+            text = getName()
+        }
+
+        Log.d("ERROR", familyName)
     }
+
+    private fun getName(): String = "$personalName $familyName"
 }
